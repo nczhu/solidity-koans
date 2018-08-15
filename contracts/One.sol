a0c55071e7acd846065deccf35611979d31acca5
Nicole Zhu <nicolezhuis@gmail.com>
2018-08-15 13:25:59 +0700

    Fix: successfully migrated the template test

--- contracts/One.sol
+++ contracts/One.sol
@@ -1,7 +1,13 @@
 pragma solidity ^0.4.24;
 
-// Make sure to 'truffle compile' after every edit!
+/* 
+ * Level 1: 
+ * Pass all the tests in TestOne.sol
+ * Make sure to truffle migrate all changes
+ */
 
 contract One {
-
+  function returnOne() public returns(uint){
+    return (1);
+  }
 }
--- test/TestOne.sol
+++ test/TestOne.sol
@@ -7,3 +7,13 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 
 import "../contracts/One.sol";
+
+contract TestOne {
+  One one = One(DeployedAddresses.One());
+
+  function testOne() public {
+    uint returnedId = one.returnOne();
+    uint expected = 1;
+    Assert.equal(returnedId, expected, "Should return 1.");
+  }
+}
