175b74f806f43b92beb828e90e04fd2c17412c8d
Nicole Zhu <nicolezhuis@gmail.com>
2018-08-15 11:43:16 +0700

    Add: empty boilerplate for the first test

---
+++ test/TestOne.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+
+// This imports truffles testing library
+import "truffle/Assert.sol";
+
+// This gets the address of test contract instance
+import "truffle/DeployedAddresses.sol";
+
+import "../contracts/One.sol";
