b2e8e2580fc59a45cac45817ed219714057eca31
Nicole Zhu <nicolezhuis@gmail.com>
2018-08-16 10:57:39 +0700

    Fix: Assert_1 tests suite is now done

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+var Assert_1 = artifacts.require("Assert_1");
+
+module.exports = function(deployer) {
+  deployer.deploy(Assert_1);
+  // Deploy more here
+};
--- test/Test_Assert_1.sol
+++ test/Test_Assert_1.sol
@@ -4,17 +4,17 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol"; // This gets the address of test contract instance
 import "../contracts/Assert_1.sol";
 
+
 contract Test_Assert_1 {
   // beforeAll()
-  
   Assert_1 instance = Assert_1(DeployedAddresses.Assert_1());
 
-  function testShouldReturnTrue() public {
-    bool actual = instance.returnTrue();
+  function testShouldReturnTrue() {
+    bool actual = instance.returnTrue();  
     Assert.isTrue(actual, "returnTrue() should return true");
   }
 
-  function testShouldReturnFalse() public {
+  function testShouldReturnFalse() {
     bool actual = instance.returnFalse();
     Assert.isFalse(actual, "returnFalse() should return false");
   }
