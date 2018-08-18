17ca1bbe9402604b71f97e0a1af51449e61ca57b
Nicole Zhu <nicolezhuis@gmail.com>
2018-08-19 00:55:51 +0800

    Add: did some planning for lvl 3

---
+++ test/Test_Bytes_3.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+import "truffle/Assert.sol";
+
+contract Test_Bytes_3 {
+    
+    function test_nil_bytes_equal_zero() public {
+
+    }
+
+    function test_bytes_default_to_32() public {
+
+    }
+
+    function test_bytes_are_bitwise() public {
+        // put more robust tests in here
+    }
+
+    function test_dynamic_byte_arrays() public {
+        // As a rule of thumb, use bytes for arbitrary-length raw byte data and string for arbitrary-length string (UTF-8) data. 
+    }
+}
--- test/Test_Integers_2.sol
+++ test/Test_Integers_2.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 import "truffle/Assert.sol";
 
 /*
- * In this level, we learn about integers in Solidity
+ * In this level, we learn about bits and ints in Solidity
  * We throw out previous conventions
  * And open our mind to new possibilities
 */
@@ -11,7 +11,7 @@ contract Test_Integers_2 {
     /* In Solidity, there's no "nil" or "null" or "undefined" default value
      * Uninitiated values are set to 0 by default.
      */
-    function test_nil_values_equal_zero() public {
+    function test_nil_ints_equal_zero() public {
         int actual;
 
         // TODO: fix this expected value
@@ -19,7 +19,7 @@ contract Test_Integers_2 {
         Assert.equal(expected, actual, "nil values should default to 0");  
     }
 
-    // Bit operators can be used to mask Solidity integers
+    // Bit operators can be used to mask Solidity variables
     function test_bitwise_operator_and() public {
         uint integer = 42;          //or 101010 bitwise
         uint mask = 56;             //or 111000 bitwise
@@ -62,7 +62,6 @@ contract Test_Integers_2 {
         int expected_diff = -1;
         Assert.equal(expected_sum, (a+b), "should correctly sum a + b");
         Assert.equal(expected_diff, (a-b), "should correctly subtract a + b");
-
     }
 
     // Observe: basic arithmetic operations no longer work as expected
