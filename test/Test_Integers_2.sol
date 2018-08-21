b2cfbccc5eac4f5e8b5babeaa0cc88708b08ad14
Nicole Zhu <nicolezhuis@gmail.com>
2018-08-21 15:09:19 +0700

    Add: test_bytes_are_bitwise to Test_Bytes_3

--- test/Test_Integers_2.sol
+++ test/Test_Integers_2.sol
@@ -8,8 +8,9 @@ import "truffle/Assert.sol";
 */
 contract Test_Integers_2 {
 
-    // Ethereum's smart contract storage slot are 256 bits each, or 32 bytes.
-    // You can save storage space (i.e. gas) by initiating ints to the minimum needed size.
+    /* Ethereum's smart contract storage slot are 256 bits each, or 32 bytes.
+     * You can save storage space (i.e. gas) by initiating ints to the minimum needed size.
+     */
     function test_ints_can_be_stored_in_various_sizes() public {
         uint8 eight_bits;
         uint16 sixteen_bits;
@@ -20,8 +21,9 @@ contract Test_Integers_2 {
         Assert.isAtLeast(max_default_bits, 115792089237316195423570985008687907853269984665640564039457584007913129639934, "the default int value should have the largest storage space");
     }
 
-    //In Solidity, there's no "nil" or "null" or "undefined" default value
-    //Uninitiated values are set to 0 by default
+    /* In Solidity, there's no "nil" or "null" or "undefined" default value
+     * Uninitiated values are set to 0 by default
+     */
     function test_nil_ints_equal_zero() public {
         int actual;
 
@@ -30,7 +32,7 @@ contract Test_Integers_2 {
         Assert.equal(expected, actual, "nil values should default to 0");  
     }
 
-    // Bit operators can be used to mask Solidity variables
+    /* Bit operators can be used to mask Solidity variables */
     function test_bitwise_operator_and() public {
         uint integer = 42;          //or 101010 bitwise
         uint mask = 56;             //or 111000 bitwise
@@ -51,7 +53,7 @@ contract Test_Integers_2 {
         Assert.equal(expected, actual, "should equal bitwise (integer XOR mask)");
     }
 
-    //  << and >> shifts the binary values
+    /*  << and >> shifts the binary values */
     function test_left_shift_operand() public {
         uint a = 555555;
         uint b = 5;
@@ -63,22 +65,23 @@ contract Test_Integers_2 {
         Assert.equal(expected, actual, "should correctly shift binary left");
     }
 
-    // Observe: basic arithmetic operations work as expected
+    /* Observe: basic arithmetic operations work as expected */
     function test_addition_and_subtraction() public {
+        
         // TODO: change the values of "a" and "b" to pass the tests
         int a = 5;
         int b = -32;
-
         int expected_sum = 3;
         int expected_diff = -1;
         Assert.equal(expected_sum, (a+b), "should correctly sum a + b");
         Assert.equal(expected_diff, (a-b), "should correctly subtract a + b");
     }
 
-    // Observe: basic arithmetic operations no longer work as expected
+    /* Observe: basic arithmetic operations no longer work as expected */
     function test_arithmetic_underflows() public {
         uint min = 0;
         uint result = min - 1;
+        
         // TODO: change the test to expect the correct outcome
         Assert.isAtMost(result, min, "result should be 0 or less than 0");
     }
@@ -86,6 +89,7 @@ contract Test_Integers_2 {
     function test_arithmetic_overflows() public {
         uint max = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
         uint result = max + 1;
+        
         // TODO: change the test to expect the correct outcome
         Assert.isAtLeast(result, max, "result should be 255 or above 255");
     }
